2f60b039e469602689813db6566843446477acd5
Esther Guo <special0831qi@gmail.com>
2020-01-29 22:12:48 +0800

    1.29

--- chap5/5.3-分数的四则运算/A.cpp
+++ chap5/5.3-分数的四则运算/A.cpp
@@ -1,43 +1,10 @@
-// 尚未通过
 // http://codeup.cn/problem.php?cid=100000590&pid=0
 
 /*
-
+使用书中方法会出现超时以及大数范围超出long long的问题……
 */
 
 #include <cstdio>
-#include <cmath>
-
-int gcd(int a, int b){
-	if (b==0) return a;
-	else return gcd(b, a%b);
-}
-
-struct Fraction {
-    int up, down; // 分子，分母
-};
-
-// 化简
-Fraction reduction(Fraction result){
-    if (result.down < 0) { // 对于负分数，统一令分子为负数
-        result.up = -result.up;
-        result.down = -result.down;
-    }
-    if (result.up == 0) result.up = 1; // 对于分子为0的分数，统一令分母为1
-    else { // 约分
-        int d = gcd(fabs(result.up), fabs(result.down)); //求出最大公约数
-        result.up /= d;
-        result.down /= d;
-    }
-    return result;
-}
-
-Fraction addition(Fraction a, Fraction b){
-	Fraction result;
-	result.up = a.up*b.down + a.down*b.up;
-	result.down = a.down*b.down;
-	return result; 
-}
 
 int main() {
 	int n;
@@ -48,21 +15,13 @@ int main() {
 				continue;	
 			}
 			else {
-				Fraction result, temp;
-				result.up = 0;
-				result.down = 1;
+				double result=0;
 				for (int i=2; i<=n; i++){
-					temp.up = 1;
-					temp.down = i;
-					for (int j=0; j<n-i+1; j++){
-						result = addition(result, temp);	
-					}
-					result = reduction(result);
+					result += (double)(n-i+1)/i;	
 				}
-				result.up *= 2;
-				double ans = (double)result.up/result.down;
-				ans += n;
-				printf("%.2lf\n", ans);
+				result *= 2;
+				result += n;
+				printf("%.2lf\n", result);
 			}
 			
 		}
--- chap5/5.3-分数的四则运算/fraction.cpp
+++ chap5/5.3-分数的四则运算/fraction.cpp
@@ -8,7 +8,7 @@ Fraction reduction(Fraction result){
         result.up = -result.up;
         result.down = -result.down;
     }
-    if (result.up == 0) result.up = 1; // 对于分子为0的分数，统一令分母为1
+    if (result.up == 0) result.down = 1; // 对于分子为0的分数，统一令分母为1
     else { // 约分
         int d = gcd(abs(result.up), abs(result.down)); //求出最大公约数
         result.up /= d;
